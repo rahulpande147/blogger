ac0b43139facb900efbd6add2d8e3f2222c8b0f4
rahulpande147 <rahulpande7242@gmail.com>
2020-10-06 10:47:08 +0530

    Initial commit

--- src/main/java/com/blogs/blogger/controllers/TempController.java
+++ src/main/java/com/blogs/blogger/controllers/TempController.java
@@ -1,9 +1,11 @@
 package com.blogs.blogger.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class TempController {
 
     @GetMapping("/admin")
